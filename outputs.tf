0bb280283f9178a266149fe4298321ca3531412a
Dennis Chong <g51683521a@gmail.com>
2023-09-17 18:07:51 +0800

    Output ec2 db and rds endpoint

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 output "aws_region" {
 	value = var.aws_region
 }
+
+output "aws_ec2_public_ip" {
+	value = "${aws_instance.public_instance.*.public_ip}"
+}
+
+output "psql_dev_db_endpoint" {
+	value = aws_db_instance.psql-dev-db.endpoint
+}
